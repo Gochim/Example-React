0702d95718d919ca2b21da0fd1935832d154acef
Dmytro Ivanchenko <54093496+Gochim@users.noreply.github.com>
2020-07-14 12:56:32 +0300

    Added layout and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,22 +10,61 @@ const movie = {
 
 class Image extends React.Component {
   render() {
-    return <img src={this.props.src} alt={this.props.alt} />;
+    return <img width="100%" src={this.props.src} alt={this.props.alt} />;
   }
 }
 
 class MovieItem extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      show: false,
+      like: false
+    };
+  }
+
+  // setState() {
+  // this.state.show = !this.state.show;
+  // }
+
+  toggleOverview = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+
+  handleLike = () => {
+    this.setState({
+      like: !this.state.like
+    });
+  };
+
   render() {
     const {
-      data: { title, vote_average, image }
+      data: { title, vote_average, image, overview }
     } = this.props;
-    console.log(this);
+    console.log(this.state);
 
     return (
-      <div>
+      <div style={{ width: "300px" }}>
         <Image src={image} alt={title} />
         <p>Title: {title}</p>
         <p>Rating: {vote_average}</p>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <button type="button" onClick={this.toggleOverview}>
+            {this.state.show ? "hide" : "show"}
+          </button>
+          <button
+            type="button"
+            className={this.state.like ? "btn--like" : ""}
+            onClick={this.handleLike}
+          >
+            Like
+          </button>
+        </div>
+
+        {this.state.show === true ? <p>{overview}</p> : null}
       </div>
     );
   }
